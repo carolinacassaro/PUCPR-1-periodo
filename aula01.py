786bbec1307acd6bd2fe4d529b79c7c501a3a0b3
carolinacassaro <carolinacassaro>
2026-03-06 10:11:50 -0300

    learning input and print

---
+++ aula01.py
@@ -0,0 +1,7 @@
+name = input("DIgite seu nome: ")
+cpf = input("DIgite seu cpf: ")
+phoneNumber = input("DIgite seu telefone: ")
+year = int(input("DIgite seu ano de nascimento: "))
+
+print(f"Nome: {name} \nCPF: {cpf} \nTelefone: {phoneNumber} \nAno de Nascimento: {year}")
+
